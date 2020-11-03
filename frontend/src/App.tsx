a80cf3a2dc4a879b1215803ef4b9a3d619529a81
Vinicius Dias <vinicius-mdias@hotmail.com>
2020-11-03 14:29:24 -0300

    Create pages and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-
+import Routes from './routes';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Routes />
       <GlobalStyle />
-      <h1>Hello World</h1>
-      <h2>Hello World</h2>
-      <h3>Hello World</h3>
-      <h4>Hello World</h4>
-      <h5>Hello World</h5>
-      <p>Hello World</p>
-    </div>
+    </>
   );
 }
 
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Home: React.FC = () => (
+  <Container>
+    <h1>Hello Dashboard</h1>
+  </Container>
+);
+
+export default Home;
