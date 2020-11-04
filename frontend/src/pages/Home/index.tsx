cb0237c20c8c20c066b3775f3c5db472e66576d0
viniciusmdias <vinicius-mdias@hotmail.com>
2020-11-04 16:28:27 -0300

    Implements add new tool funcionality

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,10 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { FormEvent, useEffect, useState, useRef } from 'react';
 import { MdAdd } from 'react-icons/md';
 import Loading from '../../components/Loading';
 import ToolsCard from '../../components/ToolsCard';
 import api from '../../services/api';
+import Button from '../../components/Button';
 
-import { Container, Header, SearchBar, ListTools } from './styles';
+import {
+  Container,
+  Header,
+  SearchBar,
+  ListTools,
+  Modal,
+  Form,
+  Error,
+} from './styles';
 
 interface toolsProps {
   id: number;
@@ -14,23 +23,24 @@ interface toolsProps {
   tags: [prop: string];
 }
 
-const config = {
-  headers: {
-    'Content-Type': 'application/json;charset=UTF-8',
-    'Access-Control-Allow-Origin': true,
-    'Access-Control-Allow-Credentials': true,
-    crossDomain: true,
-  },
-};
-
 const Home: React.FC = () => {
   const [toolsData, setToolsData] = useState<toolsProps[]>([]);
   const [checkboxSelected, setCheckboxSelected] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [form, setForm] = useState(false);
+
+  // modal
+  const [title, setTitle] = useState('');
+  const [link, setLink] = useState('');
+  const [description, setDescription] = useState('');
+  const [tags, setTags] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [tools, setTools] = useState<toolsProps[]>([]);
+  const containerRef = useRef<HTMLDivElement>(null as any);
 
   useEffect(() => {
     setLoading(true);
-    api.get('/tools', config).then((response) => {
+    api.get('/tools').then((response) => {
       setToolsData(response.data);
     });
     setLoading(false);
@@ -56,41 +66,138 @@ const Home: React.FC = () => {
     }
   }
 
+  async function handleToggleForm(event: any) {
+    event.preventDefault();
+    if (!form) {
+      containerRef.current.style.opacity = '1';
+      containerRef.current.style.visibility = 'visible';
+      return setForm(!form);
+    }
+    containerRef.current.style.opacity = '0';
+    containerRef.current.style.visibility = 'hidden';
+
+    return setForm(!form);
+  }
+
+  // modal
+  async function handleAddTool(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!title || !link || !tags) {
+      setInputError('Fill the required values');
+      return;
+    }
+
+    const tagsArray = tags.split(' ');
+
+    try {
+      const response = await api.post<toolsProps>('tools', {
+        title,
+        link,
+        description,
+        tags: tagsArray,
+      });
+
+      const responsetool = response.data;
+
+      setTools([...tools, responsetool]);
+      setTitle('');
+      setLink('');
+      setDescription('');
+      setTags('');
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
-    <Container>
-      <Header>
-        <h1>VUTTR</h1>
-        <h2>Very Useful Tools to Remember</h2>
-        <SearchBar>
-          <input
-            name="search"
-            type="text"
-            onChange={(event) => handleSearchTool(event.target.value)}
-            placeholder="search"
-          />
-          <label htmlFor="check">
-            search in tags only
+    <>
+      <Modal ref={containerRef}>
+        <Form hasError={!!inputError} onSubmit={handleAddTool}>
+          <div>
+            <MdAdd size={24} color="#000" />
+            <h3>Add new tool</h3>
+          </div>
+
+          <label htmlFor="name">
+            Tool Name (*required)
             <input
-              name="check"
-              id="check"
-              type="checkbox"
-              onChange={() => setCheckboxSelected(!checkboxSelected)}
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              name="name"
             />
           </label>
-        </SearchBar>
-        <button type="button">
-          <MdAdd size={28} color="#ffffff" />
-          Add
-        </button>
-      </Header>
-      <ListTools>
-        {loading ? (
-          <Loading />
-        ) : (
-          toolsData.map((tool) => <ToolsCard toolDetail={tool} />)
-        )}
-      </ListTools>
-    </Container>
+          <label htmlFor="link">
+            Tool Link (*required)
+            <input
+              name="link"
+              value={link}
+              onChange={(e) => setLink(e.target.value)}
+            />
+          </label>
+          <label htmlFor="description">
+            Tool Description
+            <textarea
+              name="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              rows={3}
+            />
+          </label>
+          <label htmlFor="tags">
+            Tags (*required)
+            <input
+              name="tags"
+              value={tags}
+              onChange={(e) => setTags(e.target.value)}
+              type="text"
+            />
+          </label>
+          {inputError && <Error>{inputError}</Error>}
+
+          <div className="buttons">
+            <Button type="submit">Add tool</Button>
+            <Button onClick={handleToggleForm}>Cancel</Button>
+          </div>
+        </Form>
+      </Modal>
+      <Container>
+        <Header>
+          <h1>VUTTR</h1>
+          <h2>Very Useful Tools to Remember</h2>
+          <SearchBar>
+            <input
+              name="search"
+              type="text"
+              onChange={(event) => handleSearchTool(event.target.value)}
+              placeholder="search"
+            />
+            <label htmlFor="check">
+              search in tags only
+              <input
+                name="check"
+                id="check"
+                type="checkbox"
+                onChange={() => setCheckboxSelected(!checkboxSelected)}
+              />
+            </label>
+          </SearchBar>
+          <button type="button" onClick={handleToggleForm}>
+            <MdAdd size={28} color="#ffffff" />
+            Add
+          </button>
+        </Header>
+        <ListTools>
+          {loading ? (
+            <Loading />
+          ) : (
+            toolsData.map((tool) => <ToolsCard toolDetail={tool} />)
+          )}
+        </ListTools>
+      </Container>
+    </>
   );
 };
 export default Home;
--- frontend/src/pages/Home/styles.ts
+++ frontend/src/pages/Home/styles.ts
@@ -1,4 +1,9 @@
-import styled from 'styled-components';
+/* eslint-disable prettier/prettier */
+import styled, { css } from 'styled-components';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Container = styled.main`
   display: flex;
@@ -103,4 +108,69 @@ export const ListTools = styled.section`
   display: flex;
   flex-direction: column;
   margin: 2vh 0;
+`;
+
+// modal
+export const Modal = styled.div`
+  position: fixed;
+  z-index: 2;
+  width: 100%;
+  height: 100vh;
+  opacity: 0;
+  visibility: hidden;
+  transition: all 0.6s ease-in-out;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #0000009e;
+  cursor: pointer;
+`;
+
+export const Form = styled.form<FormProps>`
+  border: 3px solid #000;
+  width: 45vw;
+  height: 80vh;
+  background-color: #fff;
+  padding: 2vh 8vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  cursor: default;
+
+  input {
+    border: 0;
+    color: #3a3a3a;
+    background: #f5f4f6 0% 0% no-repeat padding-box;
+    border: 2px solid #ebeaed;
+    border-radius: 5px;
+    padding: 1.5vh 2vh;
+    height: 50px;
+
+    ${(props) => props.hasError
+      && css`
+        border-color: #c53030;
+      `}
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin-right: 2vh;
+    }
+  }
+  label {
+    display: flex;
+    flex-direction: column;
+  }
+  .buttons {
+    justify-content: space-between;
+  }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
